c5effd74c3ba5bdc7f3deedad5e8bf89ca425406
JetsadaWijit <jetsadawijit@outlook.com>
2026-01-24 20:53:29 +0000

    chore: Create SlayerRewards.java

---
+++ src/main/java/io/github/mcclauneck/SlayerRewards.java
@@ -0,0 +1,13 @@
+package io.github.mcclauneck;
+
+import io.github.mcengine.mcextension.api.IMCExtension;
+import org.bukkit.plugin.java.JavaPlugin;
+import java.util.concurrent.Executor;
+
+public class SlayerRewards implements IMCExtension {
+    @Override
+    public void onLoad(JavaPlugin plugin, Executor executor) {}
+
+    @Override
+    public void onDisable(JavaPlugin plugin, Executor executor) {}
+}
